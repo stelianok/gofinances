1472f3b46ed3b741098e96de88126b5b8c812dd2
Kauã Steliano <stelianok@gmail.com>
2021-12-21 00:19:37 -0300

    Finished Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import {
+    Container,
+    Header,
+    Title,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    UserWrapper,
+    Icon,
+} from './styles';
 export function Dashboard() {
     return (
         <Container>
-            <Title>
-                Dashboard
-            </Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/39469125?v=4' }}
+                        />
+                        <User>
+                            <UserGreeting>
+                                Olá,
+                            </UserGreeting>
+                            <UserName>
+                                Kauã
+                            </UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
         </Container>
     );
 }
