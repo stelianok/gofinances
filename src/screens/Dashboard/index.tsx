0076843975327bf31a88fe7d0481465604f62634
Kauã Steliano <stelianok@gmail.com>
2022-01-10 19:13:31 -0300

    Created editor config

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import {
-    Container,
-    Header,
-    Title,
-    UserInfo,
-    Photo,
-    User,
-    UserGreeting,
-    UserName,
-    UserWrapper,
-    Icon,
+  Container,
+  Header,
+  Title,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  UserWrapper,
+  Icon,
 } from './styles';
 export function Dashboard() {
-    return (
-        <Container>
-            <Header>
-                <UserWrapper>
-                    <UserInfo>
-                        <Photo
-                            source={{ uri: 'https://avatars.githubusercontent.com/u/39469125?v=4' }}
-                        />
-                        <User>
-                            <UserGreeting>
-                                Olá,
-                            </UserGreeting>
-                            <UserName>
-                                Kauã
-                            </UserName>
-                        </User>
-                    </UserInfo>
-                    <Icon name="power" />
-                </UserWrapper>
-            </Header>
-        </Container>
-    );
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: 'https://avatars.githubusercontent.com/u/39469125?v=4' }}
+            />
+            <User>
+              <UserGreeting>
+                Olá,
+              </UserGreeting>
+              <UserName>
+                Kauã
+              </UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+    </Container>
+  );
 }
